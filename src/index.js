942ee5db68a386d849e1225a11c4322d7857ec6b
airtoxin <airtoxin@icloud.com>
2016-02-21 01:29:19 +0900

    Naive implementation

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import menubar from "menubar";
+import zpad from "zpad"
+import { exec } from "child_process";
+
+const FILE_DIR = "~/Desktop/sc/"
+
+const mb = menubar();
+
+let counter = 0;
+const takeScreenshot = () => {
+    counter++;
+    exec(`screencapture -x -t jpg ${FILE_DIR}test_${zpad(counter, 3)}.jpg`);
+};
+
+mb.on("ready", () => {
+    setInterval(takeScreenshot, 1000);
+});
+
+// create mov
+// ffmpeg -r 10 -i test_%3d.jpg test.mp4
