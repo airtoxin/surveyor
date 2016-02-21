2e838e3904359e9c97eb7bcf6c47858e4d03bd94
airtoxin <airtoxin@icloud.com>
2016-02-21 13:13:59 +0900

    photon

--- src/render/main/index.js
+++ src/render/main/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Photon from "react-photonkit";
 
 export default class Main extends React.Component {
     render() {
-        return (<h1>Hello!!!</h1>);
+        return (
+            <Photon.Window>
+                <Photon.Toolbar title="header" />
+                <Photon.Content />
+                <Photon.Toolbar psType="footer" title="Surveyor" />
+            </Photon.Window>
+        );
     }
 }
